2d7ce5e48b7cac0b8ba4c179dcc8e35f6cce4db6
Vlad Shesternin <valdon1202@gmail.com>
2021-06-09 22:43:52 +0300

    my lovely little creation BaseFragment - { ViewBinding } )))

--- app/src/main/java/com/example/basefragment/SomeFragment.kt
+++ app/src/main/java/com/example/basefragment/SomeFragment.kt
@@ -2,23 +2,42 @@ package com.example.basefragment
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.viewbinding.ViewBinding
+import com.example.basefragment.databinding.ActivityMainBinding
 import com.example.basefragment.databinding.FragmentSomeBinding
+import kotlin.reflect.full.declaredFunctions
+import kotlin.reflect.full.declaredMemberExtensionProperties
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.functions
 
 class SomeFragment : BaseFragment<FragmentSomeBinding, SomeViewModel>() {
 
-    override val initBinding = { FragmentSomeBinding.inflate(layoutInflater) }
-    override val initViewModel = { viewModels<SomeViewModel>().value }
+//    override val initBinding = { FragmentSomeBinding.inflate(layoutInflater) }
+//    override val initViewModel = { viewModels<SomeViewModel>().value }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private lateinit var binding: FragmentSomeBinding
 
-        printVlad(binding.tvText.text)
-        printVlad(viewModel.text)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSomeBinding.inflate(inflater)
+        printVlad("ooo Da: ${type<FragmentSomeBinding>().tvText.text}")
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        printVlad(binding.tvText.text)
+//        printVlad(viewModel.text)
+//    }
+
+    }
 }
 
 fun printVlad(it: Any) {
